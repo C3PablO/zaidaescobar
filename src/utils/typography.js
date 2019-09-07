d9e8e298e2e45a1deb0713cd2b146b57abe78f4b
Pablo Romero <pablo.romero@cantemo.com>
2019-09-07 16:14:46 +0200

    add fonts

---
+++ src/utils/typography.js
@@ -0,0 +1,35 @@
+import Typography from "typography"
+const typography = new Typography({
+  googleFonts: [
+    {
+      name: 'Abril Fatface',
+      styles: [
+        '400',
+      ],
+    },
+    {
+      name: 'Raleway',
+      styles: [
+        '300', '400',
+      ],
+    },
+  ],
+  baseFontSize: "16px",
+  baseLineHeight: 1.45,
+  scaleRatio: 4,
+  headerGray: 20,
+  bodyGray: 20,
+  headerWeight: 400,
+  bodyWeight: 300,
+  boldWeight: 400,
+  headerFontFamily: [
+    "Abril Fatface",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  bodyFontFamily: ["Raleway", "Helvetica Neue", "Segoe UI"],
+})
+export default typography
