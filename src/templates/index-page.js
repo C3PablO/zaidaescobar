46f633cff81a6620a22b192266af2019e238b10f
Pablo Romero <pablo.romero@cantemo.com>
2019-09-07 19:10:39 +0200

    modify Landing

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -5,6 +5,7 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
+import About from './about-page';
 
 export const IndexPageTemplate = ({
   title,
