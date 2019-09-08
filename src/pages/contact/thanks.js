d4e4d857489167794eb590706d4d7a036591b66b
Pablo Romero <pablo.romero@cantemo.com>
2019-09-08 18:55:46 +0200

    fix warnings

--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -7,7 +7,9 @@ export default () => (
     <section className="page-thanks">
       <div>
         <h1>¡Gracias!</h1>
-        <h2>Me pondré en contacto contigo lo antes posible 😘</h2>
+        <h2>
+          Me pondré en contacto contigo lo antes posible <span role="img" aria-label="Gracias">😘</span>
+        </h2>
         <p>Mientras tanto, echale un ojo a mi <Link to="/">obra</Link>.</p>
       </div>
     </section>
