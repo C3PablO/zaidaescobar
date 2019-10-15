3c7981941ff226e190126158bb1c73076ef95fb6
Pablo Romero <pablo.romero@cantemo.com>
2019-10-15 18:47:30 +0200

    about section fixes

--- src/components/About.js
+++ src/components/About.js
@@ -6,23 +6,21 @@ import { HTMLContent } from '../components/Content'
 class About extends PureComponent {
   render() {
     return (
-      <div className="profile--full">
-        <h1 id="about">{this.props.data.markdownRemark.frontmatter.title}</h1>
-        <div className="profile--part-wrapper">
-          <div className="profile--part profile--part__last">
-            <HTMLContent content={this.props.data.markdownRemark.html} />
-          </div>
-          <div className="profile--part">
-            <div className="profile--part--image">
-              <div className="profile--part--image--circle-wrapper">
-                <div className="profile--part--image--circle">
-                  <PreviewCompatibleImage 
-                    imageInfo={this.props.data.markdownRemark.frontmatter.image}
-                  />
-                </div>
+      <div className="profile--part-wrapper profile--full">
+        <div className="profile--part">
+          <div className="profile--part--image">
+            <div className="profile--part--image--circle-wrapper">
+              <div className="profile--part--image--circle">
+                <PreviewCompatibleImage 
+                  imageInfo={this.props.data.markdownRemark.frontmatter.image}
+                />
               </div>
             </div>
           </div>
+        </div>
+        <div className="profile--part profile--part__last">
+          <h1 id="about">{this.props.data.markdownRemark.frontmatter.title}</h1>
+          <HTMLContent content={this.props.data.markdownRemark.html} />
         </div>
       </div>
     );
