c9979cc473097bfe7c28a9cd8709d1989d67255b
Pablo Romero <pablo.romero@cantemo.com>
2019-10-15 20:17:03 +0200

    typo

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -47,7 +47,7 @@ export default class Index extends React.Component {
                 data-netlify-honeypot="bot-field"
                 onSubmit={this.handleSubmit}
               >
-                <h1 className="page-contact--title">Dejame un mensaje</h1>
+                <h1 className="page-contact--title">Déjame un mensaje</h1>
                 {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
                 <input type="hidden" name="form-name" value="contact" />
                 <input type="hidden" name="subject" value="Mensaje desde formulario de contacto" />
