c75d83009a0937afe0e294fbd9c906955eacde14
Pablo Romero <pablo.romero@cantemo.com>
2020-03-24 13:24:15 +0100

    gatsby-plugin-netlify-cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,7 @@ module.exports = {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
+        enableIdentityWidget: true,
       },
     },
     {
