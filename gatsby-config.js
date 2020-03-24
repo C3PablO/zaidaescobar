6040234963d3845670b420c7a94ed5a6e0d2ec01
Pablo Romero <pablo.romero@cantemo.com>
2020-03-24 14:52:06 +0100

    update backend

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,7 +84,6 @@ module.exports = {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
-        enableIdentityWidget: true,
       },
     },
     {
