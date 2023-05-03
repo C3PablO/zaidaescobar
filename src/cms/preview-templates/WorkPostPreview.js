d75dd9cd806559d97fd82a71b8b357668e84ebcd
Pablo Romero <pabloromerojaren@gmail.com>
2023-05-03 06:55:43 +0200

    trying something

--- src/cms/preview-templates/WorkPostPreview.js
+++ src/cms/preview-templates/WorkPostPreview.js
@@ -2,10 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { WorkPostTemplate } from '../../templates/work-page'
 
+let loaded = false;
 const BlogPostPreview = ({ entry, widgetFor }) => {
   const tags = entry.getIn(['data', 'tags'])
   const images = entry.getIn(['data', 'images'])
-
+  if (!loaded) {
+    var css = 'div[data-slate-editor] {-webkit-user-modify: read-write!important;}',
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+    head.appendChild(style);
+    style.type = 'text/css';
+    if (style.styleSheet){
+      // This is required for IE8 and below.
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+  }
+  loaded = true;
   return (
     <WorkPostTemplate
       content={widgetFor('body')}
