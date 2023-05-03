cdc4ceaf3ba24db0c7eed66557cdc9a9443665c6
Pablo Romero <pabloromerojaren@gmail.com>
2023-05-03 06:44:15 +0200

    take back

--- src/cms/preview-templates/WorkPostPreview.js
+++ src/cms/preview-templates/WorkPostPreview.js
@@ -2,14 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { WorkPostTemplate } from '../../templates/work-page'
 
-let loaded = false;
 const BlogPostPreview = ({ entry, widgetFor }) => {
   const tags = entry.getIn(['data', 'tags'])
   const images = entry.getIn(['data', 'images'])
-  if (!loaded) {
-    document.head.innerHTML += "<style>div[data-slate-editor] {-webkit-user-modify: read-write!important;}</style>";
-  }
-  loaded = true;
+
   return (
     <WorkPostTemplate
       content={widgetFor('body')}
